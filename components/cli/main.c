e6f5fb716389b9b8056a7b0b36799e3fbd63dd9f
aureoleday <ztscotty@gmail.com>
2020-05-21 16:59:06 +0800

    seperate cli module

---
+++ components/cli/main.c
@@ -0,0 +1,64 @@
+/* Console example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "cli.h"
+#include "global_var.h"
+enum
+{
+    TEST_THREAD_PRIO=3,
+    CLI_THREAD_PRIO,
+    CMD_THREAD_PRIO,
+    TCP_THREAD_PRIO,
+    SPP_THREAD_PRIO,
+    GEO_THREAD_PRIO,
+    USR_MAX_PRIO
+};
+
+#define TEST_THREAD_STACK_SIZE 	3072
+#define TCP_THREAD_STACK_SIZE 	4096
+#define CLI_THREAD_STACK_SIZE 	4096
+#define CMD_THREAD_STACK_SIZE 	3072
+#define SPP_THREAD_STACK_SIZE 	2048
+#define GEO_THREAD_STACK_SIZE 	3072
+
+static void tasks_create(void)
+{
+//    xTaskCreate(&test_thread,
+//            "Task_test",
+//            TEST_THREAD_STACK_SIZE,
+//            NULL,
+//            TEST_THREAD_PRIO,
+//            NULL);
+
+    xTaskCreate(&cli_thread,
+            "Task_cli",
+            CLI_THREAD_STACK_SIZE,
+            NULL,
+            CLI_THREAD_PRIO,
+            NULL);
+
+//    xTaskCreate(&cmd_thread,
+//            "Task_CMD",
+//            CMD_THREAD_STACK_SIZE,
+//            NULL,
+//            CMD_THREAD_PRIO,
+//            NULL);
+}
+
+void app_main()
+{
+    gvar_init();
+    gvar_register();
+    tasks_create();
+}
--- main/bak/cli.c
+++ main/bak/cli.c
@@ -120,11 +120,11 @@ static void initialize_console(void)
 #endif
 }
 
-void app_main(void)
+void cli_thread(void* param)
 {
     //initialize_nvs();
-    gvar_init();
-    gvar_register();
+    //gvar_init();
+    //gvar_register();
     //usr_sys_init()
 
 #if CONFIG_STORE_HISTORY
@@ -201,3 +201,85 @@ void app_main(void)
         linenoiseFree(line);
     }
 }
+
+//void app_main(void)
+//{
+//    //initialize_nvs();
+//    gvar_init();
+//    gvar_register();
+//    //usr_sys_init()
+//
+//#if CONFIG_STORE_HISTORY
+//    initialize_filesystem();
+//    ESP_LOGI(TAG, "Command history enabled");
+//#else
+//    ESP_LOGI(TAG, "Command history disabled");
+//#endif
+//
+//    initialize_console();
+//
+//    /* Register commands */
+//    esp_console_register_help_command();
+//    register_system();
+//    register_wifi();
+//    register_nvs();
+//
+//    /* Prompt to be printed before each line.
+//     * This can be customized, made dynamic, etc.
+//     */
+//    const char* prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
+//
+//    printf("\n"
+//           "This is an example of ESP-IDF console component.\n"
+//           "Type 'help' to get the list of commands.\n"
+//           "Use UP/DOWN arrows to navigate through command history.\n"
+//           "Press TAB when typing command name to auto-complete.\n");
+//
+//    /* Figure out if the terminal supports escape sequences */
+//    int probe_status = linenoiseProbe();
+//    if (probe_status) { /* zero indicates success */
+//        printf("\n"
+//               "Your terminal application does not support escape sequences.\n"
+//               "Line editing and history features are disabled.\n"
+//               "On Windows, try using Putty instead.\n");
+//        linenoiseSetDumbMode(1);
+//#if CONFIG_LOG_COLORS
+//        /* Since the terminal doesn't support escape sequences,
+//         * don't use color codes in the prompt.
+//         */
+//        prompt = "esp32> ";
+//#endif //CONFIG_LOG_COLORS
+//    }
+//
+//    /* Main loop */
+//    while(true) {
+//        /* Get a line using linenoise.
+//         * The line is returned when ENTER is pressed.
+//         */
+//        char* line = linenoise(prompt);
+//        if (line == NULL) { /* Ignore empty lines */
+//            continue;
+//        }
+//        /* Add the command to the history */
+//        linenoiseHistoryAdd(line);
+//#if CONFIG_STORE_HISTORY
+//        /* Save command history to filesystem */
+//        linenoiseHistorySave(HISTORY_PATH);
+//#endif
+//
+//        /* Try to run the command */
+//        int ret;
+//        esp_err_t err = esp_console_run(line, &ret);
+//        if (err == ESP_ERR_NOT_FOUND) {
+//            printf("Unrecognized command\n");
+//        } else if (err == ESP_ERR_INVALID_ARG) {
+//            // command was empty
+//        } else if (err == ESP_OK && ret != ESP_OK) {
+//            printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
+//        } else if (err != ESP_OK) {
+//            printf("Internal error: %s\n", esp_err_to_name(err));
+//        }
+//        /* linenoise allocates line buffer on the heap, so need to free it */
+//        linenoiseFree(line);
+//    }
+//}
